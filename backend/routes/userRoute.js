a450c582d060fef96c2f36670f3833ed8b5aabd2
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-18 23:22:00 +0530

    email verification added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { registerUser } from '../controllers/userController.js'
+import { registerUser, verify } from '../controllers/userController.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
+router.post('/verify-mail', verify)
 
 export default router
