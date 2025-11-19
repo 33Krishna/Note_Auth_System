848eb413a1b2f344e9f2caffaa69878d0e2b17f0
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-19 20:52:56 +0530

    logout user added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import { loginUser, registerUser, verify } from '../controllers/userController.js'
+import { loginUser, logoutUser, registerUser, verify } from '../controllers/userController.js'
+import { isAuthenticated } from '../middleware/isAuthenticated.js'
 
 const router = express.Router()
 
 router.post('/register', registerUser)
 router.post('/verify-mail', verify)
 router.post('/login', loginUser)
+router.post('/logout', isAuthenticated, logoutUser)
 
 export default router
