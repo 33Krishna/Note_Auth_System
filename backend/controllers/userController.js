e851b03c9d1891333914e3b483cca410805b9b29
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-19 21:18:32 +0530

    forgot password added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import { verifyMail } from '../email/verifyMail.js';
 import { Session } from '../models/sessionModel.js';
+import { sendOtpMail } from '../email/sendOtpMail.js';
 
 export const registerUser = async (req, res) => {
     try {
@@ -168,6 +169,36 @@ export const logoutUser = async (req, res) => {
         return res.status(200).json({
             success: true,
             message: "Logged out successfully!"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export const forgotPassword = async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await User.findOne({ email })
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User Not Found!"
+            })
+        }
+    
+        const otp = Math.floor(100000 + Math.random() * 900000).toString();
+        const expiry = new Date(Date.now() + 10 * 60 * 1000)
+    
+        user.otp = otp;
+        user.otpExpiry = expiry;
+        await user.save();
+        await sendOtpMail(email, otp);
+        return res.status(200).json({
+            success: true,
+            message: 'OTP sent successfully'
         })
     } catch (error) {
         return res.status(500).json({
