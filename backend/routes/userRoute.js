1c6842276bf112582f991969da291ee59e5d6a64
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-20 22:19:44 +0530

    yup validation added

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { changePassword, forgotPassword, loginUser, logoutUser, registerUser, verify, verifyOTP } from '../controllers/userController.js'
 import { isAuthenticated } from '../middleware/isAuthenticated.js'
+import { validateUser } from '../validators/userValidate.js'
 
 const router = express.Router()
 
-router.post('/register', registerUser)
+router.post('/register', validateUser ,registerUser)
 router.post('/verify-mail', verify)
 router.post('/login', loginUser)
 router.post('/logout', isAuthenticated, logoutUser)
