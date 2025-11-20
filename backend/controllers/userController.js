72de5dfaa6467bfd56193f43730d409327097cd3
Rati Krishna Moharana <mratikrishna@gmail.com>
2025-11-20 16:25:22 +0530

    changePassword done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -264,3 +264,46 @@ export const verifyOTP = async (req, res) => {
         })
     }
 }
+
+export const changePassword = async (req, res) => {
+    const { newPassword, confirmPassword } = req.body
+    const email = req.params.email
+
+    if(!newPassword || !confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "All fields required!"
+        })
+    }
+
+    if(newPassword !== confirmPassword) {
+        return res.status(400).json({
+            success: false,
+            message: "Password don't match"
+        })
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found!"
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10)
+        user.password = hashedPassword;
+        await user.save();
+
+        return res.status(200).json({
+            success: true,
+            message: "Password changed successfully"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: `Internal server error ${error.message}`
+        })
+    }
+}
